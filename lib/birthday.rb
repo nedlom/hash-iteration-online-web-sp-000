0b6388ff26d6e843f97103127b313f4486dadce9
imperative-scientist-4600 <nelom3@gmail.com>
2020-09-15 14:25:30 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,7 +5,9 @@
 # }
 
 def happy_birthday(birthday_kids)
-  # add your code snippet here!
+  each.birthday_kids do |kids_name, age|
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  end
 end
 
 
