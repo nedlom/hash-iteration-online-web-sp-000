dd050356cc5632cc57d4bda6a2cd1ff325e401fe
imperative-scientist-4600 <nelom3@gmail.com>
2020-09-15 14:26:48 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -6,7 +6,9 @@
 
 def happy_birthday(birthday_kids)
   each.birthday_kids do |kids_name, age|
-    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+    if age <= 12
+      puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+    end
   end
 end
 
